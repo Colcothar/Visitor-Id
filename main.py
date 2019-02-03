702cf801aaa5f99d3c98be7be472f6c73ad655f7
Colcothar <33038729+Colcothar@users.noreply.github.com>
2019-02-03 08:59:47 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import face_recognition
+import picamera
+import numpy as np
+
+# Get a reference to the Raspberry Pi camera.
+# If this fails, make sure you have a camera connected to the RPi and that you
+# enabled your camera in raspi-config and rebooted first.
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
